6414ed19a7ef4bc47c273a1c3eccf637fd83e1d6
Bart Ons <Bart.Ons@telenet.be>
2017-09-19 10:57:26 +0200

    initial sources

---
+++ trackercracker/src/main/java/be/trackercracker/trackercracker/TrackercrackerApplication.java
@@ -0,0 +1,12 @@
+package be.trackercracker.trackercracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackercrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackercrackerApplication.class, args);
+	}
+}
---
+++ trackercracker/src/test/java/be/trackercracker/trackercracker/TrackercrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package be.trackercracker.trackercracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrackercrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
